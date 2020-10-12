0c24b0110366c881a7866a78efe3168ab8c7f2cd
unknown <wjjiang19@163.com>
2020-10-12 09:18:35 +0800

    add the @MapperScan Annotation

--- src/main/java/cool/spongecaptain/springboot_mybatis/SpringbootMybatisApplication.java
+++ src/main/java/cool/spongecaptain/springboot_mybatis/SpringbootMybatisApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("cool.spongecaptain.springboot_mybatis.mapper") //与接口上的 @Mapper 注解选择其一即可
 public class SpringbootMybatisApplication {
 
 	public static void main(String[] args) {
